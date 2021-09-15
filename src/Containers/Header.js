29c8dd76323650a45f39b7074ed785f91302406a
Pavan Patwari <patwaripavan8@gmail.com>
2021-09-16 00:16:25 +0530

    Changed ProductDetail display style Added Accordion to show description

--- src/Containers/Header.js
+++ src/Containers/Header.js
@@ -3,19 +3,16 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
-  const goToHome = () => {
-    console.log("Home clicked");
-  };
-
   return (
     <div>
       <Link to="/Shopping-Cart">
-        <AppBar position="static" onClick={goToHome}>
+        <AppBar position="static">
           <Toolbar variant="dense">
             <Typography variant="h6" color="inherit">
-              Shoppers-Stop
+              <p>Shoppers-Stop</p>
             </Typography>
           </Toolbar>
         </AppBar>
--- src/Containers/ProductDetail.js
+++ src/Containers/ProductDetail.js
@@ -9,6 +9,11 @@ import {
 import "./ProductDetail.css";
 import Rating from "@material-ui/lab/Rating";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function ProductDetail() {
   const { productId } = useParams();
@@ -19,7 +24,7 @@ function ProductDetail() {
 
   //to get access to state
   const product = useSelector((state) => state.product);
-  const { id, title, price, image, category, rating } = product;
+  const { id, title, price, image, category, rating, description } = product;
   // const { rate, count } = rating;
   // console.log(rate);
 
@@ -47,19 +52,43 @@ function ProductDetail() {
     </div>
   ) : (
     <div className="productDetail">
-      <img src={image} alt="Image Not Found" className="productDetail__Image" />
-      <div className="productDetail__border"></div>
-      <div className="productDetail__title">{title}</div>
-      {/* <div className="productDetail__rating">
-        <Rating name="read-only" value={Math.round(rate)} readOnly />
-        <span>({count})</span>
-      </div> */}
-      <div className="productDetail_bottom">
-        <div className="productDetail__price">₹{price}/-</div>
-        <div className="productDetail__category">{category}</div>
+      <div className="productDetail__left">
+        <img
+          src={image}
+          alt="Image Not Found"
+          className="productDetail__Image"
+        />
+        <div className="productDetail__border"></div>
+        <div className="productDetail_title_category">
+          <div className="productDetail__title">{title}</div>
+          <div className="productDetail__category">({category})</div>
+        </div>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Product Description</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{description}</Typography>
+          </AccordionDetails>
+        </Accordion>
       </div>
-      <div className="productDetail_Button">
-        <button>Buy Now</button>
+      <div className="productDetail_bottom">
+        <div className="productDetail__right">
+          <div className="productDetail__price">
+            <strong>Price</strong> ₹{price}/-
+          </div>
+        </div>
+        <div className="productDetail__rating">
+          <Rating name="read-only" value="4" readOnly />
+          {/* <span>({count})</span> */}
+        </div>
+        <div className="productDetail_Button">
+          <button>Buy Now</button>
+        </div>
       </div>
     </div>
   );
